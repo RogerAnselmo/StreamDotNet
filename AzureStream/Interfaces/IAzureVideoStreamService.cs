8d958bf07bf23d8dfafef1e8dd1b9e65a4954abf
Rogério Anselmo <rogerio.anselmo@lambda3.com.br>
2019-04-23 15:55:47 -0300

    update gitignore

---
+++ AzureStream/Interfaces/IAzureVideoStreamService.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureStream.Interfaces
+{
+    public interface IAzureVideoStreamService
+    {
+        Task<Stream> GetVideoByName(string name);
+    }
+}
